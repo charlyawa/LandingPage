42203e0635da4f57d050177b3199e1658f5abcb2
charlyawa <alvacharly31@gmail.com>
2021-04-26 02:11:00 -0600

    mandar correos

--- send.php
+++ send.php
@@ -1,22 +1,15 @@
 <?php
-$name = $_POST['name'];
-$mail = $_POST['mail'];
-$message = $_POST['message'];
+    $destinatario = 'alvacharly31@gmail.com';
+    $name = $_POST['name'];
+    $mail = $_POST['mail'];
+    $message = $_POST["message"];
 
-$header = 'From: ' . $mail . " \r\n";
-$header = "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header = "Mime-Version: 1.0  \r\n";
-$header = "Content-Type: text/plain";
 
-$message = "Message sent by: " . $name . " \r\n";
-$message = "Your email is: " . $mail . " \r\n";
-$message = "Message: " . $_POST['message'] . " \r\n";
-$message = "Shipping date: " .date('d/m/Y', time());
+    $header = "Enviado desde la pagina CarlosCs";
+    $messageComplete = $message . "\nAtentamente: " . $name;
 
-$para = 'alvacharly31@gmail.com';
-$asunto = 'Asunto del mensaje';
+    mail($destinatario, $messageComplete, $header);
+    echo "<script>alert('correo enviado exitosamente')</script>";
+    echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
 
-mail($para, $asunto, utf8_decode($message), $header);
-
-header("Location:index.html");
 ?>
